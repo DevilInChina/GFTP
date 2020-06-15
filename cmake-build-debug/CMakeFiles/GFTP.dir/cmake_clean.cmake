ec06494dc8270373177e543c5dde3e352afb44a6
DevilInChina <905320627@qq.com>
2020-06-15 17:03:44 +0800

    Clions' Init

---
+++ cmake-build-debug/CMakeFiles/GFTP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GFTP.dir/main.cpp.obj"
+  "GFTP.exe"
+  "GFTP.exe.manifest"
+  "GFTP.lib"
+  "GFTP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GFTP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/GFTP.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GFTP.
+# This may be replaced when dependencies are built.
