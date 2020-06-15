c9582831bd04c5844b546f56c8b0256c4b27e1d6
DevilInChina <905320627@qq.com>
2020-06-15 21:45:46 +0800

    windows send test

--- main.cpp
+++ main.cpp
@@ -2,52 +2,26 @@
 #include "Connector.h"
 char buff[1024];
 int main(int argc,char **argv) {
-    Connector s;
 #ifdef WIN32
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 #endif
 
     if(argc > 1){
-        int k = s.CreateSocket("192.168.2.108",1453);
+        ftpServer s("192.168.2.100",1453);
+        cout<<"start linsen "<<s._socket<<endl;
+        s.beginListen();
     }else{
-        CurSocket sclient = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        if(sclient == INVALID_SOCKET)
-        {
-            printf("invalid socket!");
-            return 0;
-        }
-
-        sockaddr_in serAddr;
-        serAddr.sin_family = AF_INET;
-        serAddr.sin_port = htons(1453);
-        serAddr.sin_addr.s_addr = inet_addr("192.168.2.100");
-        if(connect(sclient, (sockaddr *)&serAddr, sizeof(serAddr))<0)
-        {  //连接失败
-            printf("connect error !");
-            close(sclient);
-            return 0;
-        }
-
+        ftpClient s("192.168.2.100",1453);
         string data;
-        cin>>data;
-        const char * sendData;
-        sendData = data.c_str();   //string转const char*
-        //char * sendData = "你好，TCP服务端，我是客户端\n";
-        send(sclient, sendData, strlen(sendData), 0);
-        //send()用来将数据由指定的socket传给对方主机
-        //int send(int s, const void * msg, int len, unsigned int flags)
-        //s为已建立好连接的socket，msg指向数据内容，len则为数据长度，参数flags一般设0
-        //成功则返回实际传送出去的字符数，失败返回-1，错误原因存于error
-
-        char recData[255];
-        int ret = recv(sclient, recData, 255, 0);
-        if(ret>0){
-            recData[ret] = 0x00;
-            printf(recData);
+        while (true){
+            cin>>data;
+            s.Send(data);
+            cout<<"senddone"<<endl;
         }
-        close(sclient);
-     }
-    //WSACleanup();
+    }
+#ifdef WIN32
+    WSACleanup();
+#endif
     return 0;
 }
